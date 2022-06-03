5d275c298c079856dae60e673ff057f4947e132b
Carlos Ramirez <karlossander92@gmail.com>
2022-06-02 21:08:22 -0500

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header(props){
     console.log(props);
     return(
         <>
-            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes{' '}<span className="text-indigo-600">Veterinaria</span></h1>
+            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes{' '}Con Githubñ¿{''}<span className="text-indigo-600">Veterinaria</span></h1>
         </>
     )
 }
